071bc30f16d4002025892c7340d9a2fe0696b4d4
Saviour1001 <harshghodkar@gmail.com>
2021-01-24 19:10:40 +0530

    Updated the files

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Harsh Ghodkar",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Harsh Ghodkar and I am a Software Engineer!",
+  githubUrl: "https://github.com/Saviour1001",
+  linkedinUrl: "https://www.linkedin.com/in/harsh-ghodkar/",
+  cvLink: "https://drive.google.com/file/d/1VPCOJ7GRcIvF3vVnm3y3bV8UveRX8ZxD/view?usp=sharing",
+};
